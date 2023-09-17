9166fce4474158267ae63613a7b4d81b02bfdfb2
Aaronramirezrob <germanramirezrob@gmail.com>
2023-09-17 17:13:56 -0500

    Modificaciones agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { GifsModule } from './gifs/gifs.module';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+
     GifsModule,
-    HttpClientModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
